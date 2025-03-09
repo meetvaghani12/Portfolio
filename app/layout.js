fc55e7cc813c889ef7203e1240f5232b70c4e83d
Meet Vaghani <meetvaghani1238@gmail.com>
2025-03-09 18:41:44 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-//"use client";
+"use client";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,11 +9,11 @@ import "./css/card.scss";
 import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-title: "Portfolio of Meet Vaghani - Software Developer",
-description:
-"This is the portfolio of Meet Vaghani. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
-};
+// export const metadata = {
+// title: "Portfolio of Meet Vaghani - Software Developer",
+// description:
+// "This is the portfolio of Meet Vaghani. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+// };
 
 export default function RootLayout({ children }) {
 return (
