9ab1a80b7f172b6c6235a56d6eb7fbf68639a9f8
Meet Vaghani <meetvaghani1238@gmail.com>
2025-03-09 20:10:12 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client"
+import { useEffect, useState } from "react";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -7,19 +8,28 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-
-export default async function Home() {
+export default function Home() {  // Removed 'async'
+  const [isMounted, setIsMounted] = useState(false);
   
-
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  
   return (
-    <div suppressHydrationWarning >
-      <HeroSection />
-      <AboutSection />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Education />
-      <ContactSection />
+    <div suppressHydrationWarning>
+      {isMounted ? (
+        <>
+          <HeroSection />
+          <AboutSection />
+          <Experience />
+          <Skills />
+          <Projects />
+          <Education />
+          <ContactSection />
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   )
 };
