7c007ebe48d1891cc9fd27bc496f3955afa1a7de
Meet Vaghani <meetvaghani1238@gmail.com>
2026-01-06 12:10:23 +0530

    Update experience entries with new roles and durations

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,28 +1,30 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Web Developer Intern',
-    company: "Superior Exports Limited.",
-    duration: "(May 2024 - July 2024)"
+    title: "SDE-1",
+    company: "devx AI labs",
+    duration: "(Jan 2026 - Present)"
+   
   },
-  {
+   {
     id: 2,
+    title: "Full Stack Developer",
+    company: "AppArrow Infotech",
+    duration: "(Dec 2024 - Jan 2026)"
+   
+  },
+  
+  {
+    id: 3,
     title: "Research Intern",
     company: "Woosong University(South Korea)",
     duration: "(May 2024 - Sep 2024)"
    
   },
   {
-    id: 2,
-    title: "Full Stack Developer",
-    company: "AppArrow Infotech",
-    duration: "(Dec 2024 - Present)"
-   
-  },
-  {
-    id: 3,
-    title: "FullStack Developer",
-    company: "Freelancer",
-    duration: "(Feb 2023 - Present)"
+    id: 4,
+    title: 'Web Developer Intern',
+    company: "Superior Exports Limited.",
+    duration: "(May 2024 - July 2024)"
   }
 ]
